61ffeb05504cc924c41b90a29a855f052d818bc4
Antti Vikman <antti.vikman@nolwenture.com>
2021-06-07 15:47:53 +0300

    Example of calculator and run jsbackend tests on each commit

--- jsbackend/tdd/calculator.js
+++ jsbackend/tdd/calculator.js
@@ -1,3 +1,24 @@
-export const convertStringToNumber = (string) => {};
-export const convertNumberToString = (number) => {};
-export const sum = (a, b) => {};
+const dictionary = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+];
+
+export const convertStringToNumber = (string) => {
+  return dictionary.indexOf(string.toLowerCase());
+};
+export const convertNumberToString = (number) => {
+  return dictionary[number];
+};
+export const sum = (a, b) => {
+  const result = convertStringToNumber(a) + convertStringToNumber(b);
+  return convertNumberToString(result);
+};
